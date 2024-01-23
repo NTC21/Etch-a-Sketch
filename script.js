56811efde2dc1d5223dfc626255900ad49dffa4d
Nathan Coleman <92738130+NTC21@users.noreply.github.com>
2024-01-22 23:37:48 -0500

    added style. Pixel Control Slider Created

--- script.js
+++ script.js
@@ -1,54 +1,39 @@
 let container = document.querySelector("#container");
+let squares = 16;
 
 
-function handlemousedown(){
-    isMouseDown = true;
+function mousehandlle (){
+
 }
 
-function handlemouseup() {
-    isMouseDown = false;
-}
-
-function handlemouseover() {
-    if (isMouseDown) {
-        this.classList.add('hover');
+function createPixels() {
+    // Clear existing grid
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
     }
-}
-container.addEventListener("mousedown", handlemousedown);
-container.addEventListener("mouseup", handlemouseup);
 
+    for (let i = 0; i < squares; i++) {
+        let container1 = document.createElement('div');
+        container.appendChild(container1);
+        container1.classList.add('container1');
 
-for (let i =0; i<16;i++){
+        for (let j = 0; j < squares; j++) {
+            let box = document.createElement('div');
+            box.classList.add('box');
+            container1.appendChild(box);
 
-    let container1 = document.createElement('div');
-    container.appendChild(container1);
-    container1.classList.add('container1');
-
-    for (let j =0; j<16; j++){
-        let box = document.createElement('div');
-        box.classList.add('box');
-
-        box.addEventListener("mouseover", handlemouseover);
-
-
-        container1.appendChild(box);
-
+        }
     }
 }
 
+createPixels();
 
+// Adjusting number of pixels
+let input = document.querySelector('.pixels');
 
+input.addEventListener('input', () => {
+    squares = input.value;
+    createPixels();
+});
 
-
-
-
-
-// adjusting number of pixels
-
-// let btn =document.querySelector('pixels');
-
-// btn.addEventListener('click', () =>{
-
-// let squares = prompt("Enter the number of squares you want");
-// }
-// )
+// 
